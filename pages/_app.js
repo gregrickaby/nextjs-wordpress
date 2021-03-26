6566a10c534ef95a8498a0ac4a62b303e036a48b
Greg Rickaby <gregrickaby@gmail.com>
2021-03-26 08:15:09 -0500

    use <ApolloProvider>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import {client} from '@/lib/wordpress/client'
 import '@/styles/globals.css'
+import {ApolloProvider} from '@apollo/client'
 
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
