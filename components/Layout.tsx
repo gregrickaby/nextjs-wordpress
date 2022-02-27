ca3a814448a6dc6fd67c120de082b01ee34354a5
Greg Rickaby <gregrickaby@gmail.com>
2022-02-26 21:52:46 -0600

    create and use <WordpressProvider>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,22 @@
 import Head from 'next/head'
+import {useWordPressContext} from '~/components/WordpressProvider'
 import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout({children}) {
+  const {data} = useWordPressContext()
+
   return (
     <div className="container m-auto my-8 space-y-8">
       <Head>
-        <title>Site Title</title>
-        <meta name="description" content="Description" />
+        <title>{data?.generalSettings?.title}</title>
+        <meta name="description" content={data?.generalSettings?.description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>{children}</main>
       <Footer />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import {ApolloProvider} from '@apollo/client'
 import type {AppProps} from 'next/app'
+import WordpressProvider from '~/components/WordpressProvider'
 import {client} from '~/lib/wordpressClient'
 import '~/styles/globals.css'
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <WordpressProvider>
+        <Component {...pageProps} />
+      </WordpressProvider>
     </ApolloProvider>
   )
 }
