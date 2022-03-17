4f5fd1b9155de39179eef10eb9c66f879e03f38c
Greg Rickaby <gregrickaby@gmail.com>
2022-03-17 18:20:21 -0500

    ignore img

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'jsx-a11y/anchor-is-valid': 'off',
+    '@next/next/no-img-element': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
     'prettier/prettier': 'error'
   }
