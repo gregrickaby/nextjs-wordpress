7aae4dc27bbd01b25b36cdaf65fc83123312a292
Greg Rickaby <gregrickaby@gmail.com>
2022-03-22 17:34:29 -0500

    a11y. comment code.

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -1,49 +1,53 @@
 import {useMutation} from '@apollo/client'
 import {useForm} from 'react-hook-form'
+import Comment from '~/components/Comment'
 import {CREATE_COMMENT} from '~/lib/mutations'
 
-export default function CommentForm() {
+export default function CommentForm({postId}: {postId: number}) {
   const {register, handleSubmit} = useForm()
   const [addComment, {error, data}] = useMutation(CREATE_COMMENT)
 
-  if (error) {
-    return (
-      <div>
-        <h2 className="text-red-500">Error!</h2>
-        {!!error.graphQLErrors &&
-          error.graphQLErrors.length >= 1 &&
-          error.graphQLErrors.map((error, index) => (
-            <p key={index} className="font-mono text-red-500">
-              {error.message}
-            </p>
-          ))}
-      </div>
-    )
-  }
-
+  // Form submission handler.
   function onSubmit(form) {
     addComment({
       variables: {
-        authorName: form.authorName,
-        authorEmail: form.authorEmail,
+        authorName: form?.authorName,
+        authorEmail: form?.authorEmail,
         authorUrl: form?.authorUrl,
-        comment: form.comment,
-        postID: 1
+        comment: form?.comment,
+        postID: postId
       }
     })
   }
 
+  // If there's an error, display it.
+  if (error) {
+    return (
+      <>
+        {!!error?.graphQLErrors &&
+          error?.graphQLErrors?.length >= 1 &&
+          error?.graphQLErrors?.map((error, index) => (
+            <p key={index} className="font-mono text-red-500">
+              {error?.message}
+            </p>
+          ))}
+      </>
+    )
+  }
+
+  // If the comment was created successfully, show the comment.
   if (data) {
     return (
-      <div>
-        <h2 className="text-green-500">Success!</h2>
-        <p className="font-mono text-green-500">
-          {data.createComment.success
-            ? 'Your comment has been created'
-            : 'There was an error creating your comment'}
-        </p>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      <ol>
+        {data?.createComment?.success &&
+        data?.createComment?.comment != null ? (
+          <li>
+            <Comment {...data?.createComment?.comment} />
+          </li>
+        ) : (
+          <p>Your comment will be published after moderation.</p>
+        )}
+      </ol>
     )
   }
 
@@ -51,26 +55,43 @@ export default function CommentForm() {
     <>
       <h2>Leave a comment</h2>
       <form
-        onSubmit={handleSubmit(onSubmit)}
         className="flex max-w-md flex-col space-y-4"
         name="comment-form"
+        onSubmit={handleSubmit(onSubmit)}
       >
         <input
           {...register('authorName', {required: true})}
+          aria-label="Your first and last name"
           className="field"
+          placeholder="Jane Doe"
+          required
           type="text"
         />
         <input
           {...register('authorEmail', {required: true})}
+          aria-label="Your email address"
           className="field"
+          placeholder="email@example.com"
+          required
           type="email"
         />
-        <input {...register('authorUrl')} className="field" type="url" />
+        <input
+          {...register('authorUrl')}
+          aria-label="Your website address"
+          className="field"
+          placeholder="https://example.com"
+          type="url"
+        />
         <textarea
           {...register('comment', {required: true})}
+          aria-label="Add your comment"
           className="field"
+          placeholder="Add your comment"
+          required
         />
-        <button type="submit">Submit</button>
+        <button aria-label="Click to submit your comment" type="submit">
+          Submit
+        </button>
       </form>
     </>
   )
