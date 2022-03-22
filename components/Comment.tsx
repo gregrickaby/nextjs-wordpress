67058244768e57b71433c60b5a06f2893dc7a4d4
Greg Rickaby <gregrickaby@gmail.com>
2022-03-22 17:34:07 -0500

    add single comment

---
+++ components/Comment.tsx
@@ -0,0 +1,24 @@
+import {CommentFields} from '~/lib/types'
+
+export default function Comment(comment: CommentFields) {
+  return (
+    <article>
+      <header>
+        <img
+          alt={comment?.author?.node?.name}
+          height="50"
+          loading="lazy"
+          src={comment?.author?.node?.gravatarUrl}
+          width="50"
+        />
+        <h3>
+          <a href={comment?.author?.node?.url} rel="external nofollow">
+            {comment?.author?.node?.name}
+          </a>
+          <time>{comment?.date}</time>
+        </h3>
+      </header>
+      <main dangerouslySetInnerHTML={{__html: comment?.content}} />
+    </article>
+  )
+}
