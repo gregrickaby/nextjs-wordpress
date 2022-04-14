a8dec72e82c66974e21b62ccbeab788cc415abd9
Greg Rickaby <gregrickaby@gmail.com>
2022-04-14 17:22:57 -0500

    add tailwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}']
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  plugins: [require('@tailwindcss/typography')]
 }
