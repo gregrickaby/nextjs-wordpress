511a622cbd6f5116d3478c27c1e08eced16e8153
Greg Rickaby <gregrickaby@gmail.com>
2022-04-14 18:05:19 -0500

    add tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,5 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 }
