33e3650c3f85eeb84975a79153c6f80073ceb5d3
Greg Rickaby <gregrickaby@gmail.com>
2022-10-08 13:38:19 -0500

    init mantine

--- apps/nextjs/pages/_app.tsx
+++ apps/nextjs/pages/_app.tsx
@@ -1,11 +1,40 @@
 import {ApolloProvider} from '@apollo/client'
+import {ColorScheme, ColorSchemeProvider, MantineProvider} from '@mantine/core'
+import {useColorScheme, useHotkeys, useLocalStorage} from '@mantine/hooks'
 import type {AppProps} from 'next/app'
 import {client} from '~/lib/helpers'
 
+/**
+ * Custom App component.
+ *
+ * @see https://nextjs.org/docs/advanced-features/custom-app
+ * @see https://mantine.dev/guides/next/
+ * @see https://www.apollographql.com/docs/react/api/react/hooks/#the-apolloprovider-component
+ */
 export default function App({Component, pageProps}: AppProps) {
+  const preferredColorScheme = useColorScheme()
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'nextjs-wp-color-scheme',
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true
+  })
+
+  function toggleColorScheme(value?: ColorScheme) {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+  }
+
+  useHotkeys([['mod+j', () => toggleColorScheme()]])
+
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
