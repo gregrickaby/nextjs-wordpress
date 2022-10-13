6473481166a3bc678e972bafb91d9a1b64c81584
Greg Rickaby <gregrickaby@gmail.com>
2022-10-13 13:19:06 -0500

    add flatListToHierarchical helper

--- apps/nextjs/lib/helpers.ts
+++ apps/nextjs/lib/helpers.ts
@@ -34,3 +34,29 @@ export function sanitizeComment(dirtyComment: string) {
 
   return parse(DOMPurify.sanitize(dirtyComment))
 }
+
+/**
+ * Convert a flat list to a tree.
+ *
+ * @see https://www.wpgraphql.com/docs/menus/#hierarchical-data
+ */
+export function flatListToHierarchical(
+  data = [],
+  {idKey = 'key', parentKey = 'parentId', childrenKey = 'children'} = {}
+) {
+  const tree = []
+  const childrenOf = {}
+
+  // Loop through each item in the list.
+  data.forEach((item) => {
+    const newItem = {...item}
+    const {[idKey]: id, [parentKey]: parentId = 0} = newItem
+    childrenOf[id] = childrenOf[id] || []
+    newItem[childrenKey] = childrenOf[id]
+    parentId
+      ? (childrenOf[parentId] = childrenOf[parentId] || []).push(newItem)
+      : tree.push(newItem)
+  })
+
+  return tree
+}
