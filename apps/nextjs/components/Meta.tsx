0dc294d9782e6ca6a096bcadee16ef4628505997
Greg Rickaby <gregrickaby@gmail.com>
2022-10-13 16:52:46 -0500

    add viewport

--- apps/nextjs/components/Meta.tsx
+++ apps/nextjs/components/Meta.tsx
@@ -8,6 +8,10 @@ import Head from 'next/head'
 export default function Meta() {
   return (
     <Head>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
       <link
         rel="preconnect"
         href={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}`}
