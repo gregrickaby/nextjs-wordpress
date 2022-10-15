98c4761dd4696e1dc73a3307e9a9d5988b41568b
Greg Rickaby <gregrickaby@gmail.com>
2022-10-14 21:23:16 -0500

    use container

--- apps/nextjs/components/Layout.tsx
+++ apps/nextjs/components/Layout.tsx
@@ -1,4 +1,4 @@
-import {createStyles} from '@mantine/core'
+import {Container, createStyles} from '@mantine/core'
 import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import Meta from '~/components/Meta'
@@ -6,10 +6,6 @@ import {ChildrenProps} from '~/lib/types'
 
 const useStyles = createStyles((theme) => ({
   container: {
-    margin: '0 auto',
-    maxWidth: '811px',
-    padding: `0 ${theme.spacing.xl}px`,
-
     '& > *': {
       marginBottom: theme.spacing.xl,
       marginTop: theme.spacing.xl
@@ -24,11 +20,11 @@ export default function Layout({children}: ChildrenProps) {
   const {classes} = useStyles()
 
   return (
-    <div className={classes.container}>
+    <Container className={classes.container}>
       <Meta />
       <Header />
       <main>{children}</main>
       <Footer />
-    </div>
+    </Container>
   )
 }
