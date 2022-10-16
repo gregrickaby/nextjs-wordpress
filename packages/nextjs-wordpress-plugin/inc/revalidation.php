f8914756c424de22cd644d635fc77b616be6b22b
Greg Rickaby <gregrickaby@gmail.com>
2022-10-15 19:51:09 -0500

    use transition_post_status

--- packages/nextjs-wordpress-plugin/inc/revalidation.php
+++ packages/nextjs-wordpress-plugin/inc/revalidation.php
@@ -18,7 +18,7 @@ use \WP_Post;
  * Including: the post status, comments, meta, terms, etc.
  * The slug MUST have a leading slash and cannot have a railing slash!
  *
- * @usage https://gregrickaby.com/api/wordpress/revalidate?token=123&slug=/the-slug
+ * @usage https://nextjswp.com/api/wordpress/revalidate?token=123&slug=/the-slug
  *
  * @see https://developer.wordpress.org/reference/hooks/edit_post/
  * @see https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
@@ -41,30 +41,47 @@ function on_demand_revalidation( $post_ID, WP_Post $post ) {
 
 	// Build and format the slug.
 	$slug = \get_the_permalink( $post_ID );
-	$slug = str_replace( home_url(), '', $slug );
+	$slug = str_replace( \home_url(), '', $slug );
 	$slug = ltrim( $slug, '/' );
 	$slug = rtrim( $slug, '/' );
 	$slug = '/' . $slug;
 
 	// Build the API URL.
-	$api_url = HEADLESS_FRONTEND_URL . '/api/wordpress/revalidate';
+	$api_url = HEADLESS_FRONTEND_URL . 'api/wordpress/revalidate';
 	$api_url = \add_query_arg( 'token', PREVIEW_SECRET_TOKEN, $api_url );
 	$api_url = \add_query_arg( 'slug', $slug, $api_url );
 
 	// Make API request.
-	$response = \wp_remote_post( esc_url_raw( $api_url ), [ 'blocking' => true ] );
+	$response = \wp_remote_post(
+		\esc_url_raw( $api_url ),
+		[
+			'blocking' => true,
+		]
+	);
 
 	// Check the response.
-	$response_code    = \wp_remote_retrieve_response_code( $response );
-	$response_message = \wp_remote_retrieve_response_message( $response );
-
-	// Error handle the response.
-	if ( 200 !== $response_code && ! empty( $response_message ) ) {
-		return new \WP_Error( 'grd-headless-core-revalidation-error', $response_message );
-	} elseif ( 200 !== $response_code ) {
-		return new \WP_Error( 'grd-headless-core-revalidation-error', sprintf( 'GRD Headless Core: Error %d: %s', $response_code, $response_message ) );
-	} else {
-		return;
+	if ( is_wp_error( $response ) ) {
+		$error_message = $response->get_error_message();
+		error_log( 'Revalidation error: ' . $error_message ); // phpcs:ignore
 	}
 }
-add_action( 'edit_post', __NAMESPACE__ . '\on_demand_revalidation', 10, 3 );
+
+/**
+ * Handle post status transition.
+ *
+ * @param string $new_status New status.
+ * @param string $old_status Old status.
+ * @param object $post Post object.
+ * @since 1.0.0
+ * @author Greg Rickaby
+ */
+function transition_handler( $new_status, $old_status, $post ) {
+	// If the post is a draft, bail.
+	if ( 'draft' === $new_status && 'draft' === $old_status ) {
+		return;
+	}
+
+	// Otherwise, revalidate.
+	on_demand_revalidation( $post->ID, $post );
+}
+\add_action( 'transition_post_status', __NAMESPACE__ . '\transition_handler', 10, 3 );
