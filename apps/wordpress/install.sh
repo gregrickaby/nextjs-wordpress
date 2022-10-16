622c1e4109179aac1be6ef49181cebbb0f4d7c84
Greg Rickaby <gregrickaby@gmail.com>
2022-10-16 14:53:32 -0500

    leverage local composer

--- apps/wordpress/install.sh
+++ apps/wordpress/install.sh
@@ -8,6 +8,11 @@ echo -e "Reading ENV vars..."
 source .env
 sleep 1
 
+echo -e "Running Composer..."
+cd wp-content
+composer install --prefer-dist --no-dev
+cd ..
+
 echo -e "Updating hosts file. You may be prompted for a password!"
 echo "127.0.0.1 ${WORDPRESS_URL}" | sudo tee -a /etc/hosts
 sleep 1
