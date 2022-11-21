4968b7aea8a91bba878f77c2b55414d1dacd390b
Greg Rickaby <gregrickaby@gmail.com>
2022-11-21 07:51:10 -0600

    dependency bump

--- dependabots.sh
+++ dependabots.sh
@@ -3,9 +3,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Dependabots
 cd ./ && git pull
 cd ./apps/nextjs && npx npm-check-updates -u
-cd ../wordpress && composer upgrade
-cd ../../packages/eslint-config && npx npm-check-updates -u
-cd ../prettier-config && npx npm-check-updates -u
+cd ../wordpress/wp-content && composer upgrade
+cd ../../../packages/eslint-config-custom && npx npm-check-updates -u
+cd ../prettier-config-custom && npx npm-check-updates -u
 cd ../../ && composer upgrade && npx npm-check-updates -u && npm i
 cd ./ && git add . && git commit -m "dependency bump" && git push
 echo Done!
