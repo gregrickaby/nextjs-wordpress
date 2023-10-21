2943c6e8827cebc5e2e718e34a3c32bd309ad1a0
Greg Rickaby <gregrickaby@gmail.com>
2023-10-21 16:01:41 -0500

    add on demand revalidation support

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,19 @@
+import {revalidateTag} from 'next/cache'
+
+/**
+ * On-demand revalidation endpoint.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#on-demand-revalidation
+ */
+export async function POST(request: Request) {
+  const requestHeaders = new Headers(request.headers)
+  const secret = requestHeaders.get('x-vercel-revalidation-secret')
+
+  if (secret !== process.env.REVALIDATE_SECRET) {
+    return Response.json({message: 'Invalid secret'}, {status: 401})
+  }
+
+  revalidateTag('posts')
+
+  return Response.json({revalidated: true, now: Date.now()})
+}
