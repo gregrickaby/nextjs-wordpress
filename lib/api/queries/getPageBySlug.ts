345f604f3210952cf719d9d727cbd6f18cda08fe
Greg Rickaby <gregrickaby@gmail.com>
2023-10-22 07:18:06 -0500

    create get page by slug query

---
+++ lib/api/queries/getPageBySlug.ts
@@ -0,0 +1,48 @@
+import {fetchGraphQL} from '@/lib/functions'
+import {Page} from '@/lib/types'
+
+/**
+ * Fetch a page by slug.
+ */
+export async function getPageBySlug(slug: string) {
+  const query = `
+    query GetPageBySlug($slug: ID = "URI") {
+      page(idType: URI, id: $slug) {
+        databaseId
+        content(format: RENDERED)
+        title(format: RENDERED)
+        featuredImage {
+          node {
+            altText
+            mediaDetails {
+              sizes(include: MEDIUM) {
+                height
+                width
+                sourceUrl
+              }
+            }
+          }
+        }
+        author {
+          node {
+            gravatarUrl
+            name
+          }
+        }
+        date
+        seo {
+          metaDesc
+          title
+        }
+      }
+    }
+  `
+
+  const variables = {
+    slug: slug
+  }
+
+  const response = await fetchGraphQL(query, variables)
+
+  return response.data.page as Page
+}
