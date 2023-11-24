da5e81cce19b8fc27cdae44934615b0d3b7e5440
Greg Rickaby <gregrickaby@gmail.com>
2023-11-24 13:07:12 -0600

    update import. add viewport config. add jsdocs.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
-import '@/app/globals.css'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-import type {Metadata} from 'next'
+import type {Metadata, Viewport} from 'next'
+import './globals.css'
 
+/**
+ * Setup metadata.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+ */
 export const metadata: Metadata = {
   title: 'Next.js WordPress',
   description: "It's headless WordPress"
+}
+
+/**
+ * Setup viewport.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-viewport
+ */
+export const viewport: Viewport = {
+  colorScheme: 'dark',
+  themeColor: 'black'
 }
 
 /**
