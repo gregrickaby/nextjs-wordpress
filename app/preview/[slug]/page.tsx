68e924546fe36ba8b6cd7138894b61b027eb644e
Greg Rickaby <gregrickaby@gmail.com>
2023-12-19 08:13:56 -0600

    remove comment section from preview

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import config from '@/lib/config'
 import getPreview from '@/lib/queries/getPreview'
 import {Metadata} from 'next'
-import Image from 'next/image'
 
 // Types.
 interface PreviewProps {
@@ -139,31 +138,6 @@ export default async function Preview({params, searchParams}: PreviewProps) {
           </ul>
         </div>
       </footer>
-      <section className="border-t-2">
-        <h3>Comments</h3>
-        {post.comments.nodes.map((comment) => (
-          <article key={comment.databaseId}>
-            <header className="flex items-center gap-2">
-              <Image
-                alt={comment.author.node.name}
-                className="m-0 rounded-full"
-                height={64}
-                src={comment.author.node.gravatarUrl}
-                width={64}
-              />
-              <div className="flex flex-col gap-2">
-                <h4
-                  className="m-0 p-0 leading-none"
-                  dangerouslySetInnerHTML={{__html: comment.author.node.name}}
-                />
-                <time className="italic">{comment.date}</time>
-              </div>
-            </header>
-
-            <div dangerouslySetInnerHTML={{__html: comment.content}} />
-          </article>
-        ))}
-      </section>
     </article>
   )
 }
