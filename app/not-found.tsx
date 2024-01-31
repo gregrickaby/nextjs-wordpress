5c875d016d589dc61994872792a7c21341bd4e43
Greg Rickaby <gregrickaby@gmail.com>
2024-01-31 08:37:33 -0600

    display route on 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,18 @@
+import {headers} from 'next/headers'
+
 /**
  * Not Found component.
  *
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/not-found
  */
-export default function NotFound() {
+export default async function NotFound() {
+  const headersList = headers()
+  const referer = headersList.get('referer')
+
   return (
-    <div className="flex min-h-[5rem] items-center justify-center text-xl font-bold">
-      ⚠️ Post not found!
-    </div>
+    <>
+      <h1 className="text-center">404 - Not Found</h1>
+      <p className="text-center text-red-500">{referer}</p>
+    </>
   )
 }
